440e299e02cf3e29ca61256d5c408f77eea63c61
Tim Fox <tim@elephantscale.com>
2018-02-06 10:11:35 -0500

    Fixed investment valuation case classes lab [2]

--- 07-caseclasses/src/main/scala/example/InvestmentValuation.scala
+++ 07-caseclasses/src/main/scala/example/InvestmentValuation.scala
@@ -1,9 +1,20 @@
 package example
 
-object InvestmentValuation extends Goal with App {
-  println(goal)
-}
+object InvestmentValuation   {
 
-trait Goal {
-  lazy val goal: String = "use case classes"
+  def main(args: Array[String]) {
+    println("Make some offers here)
+
+    val offerBitCoin = getOffer(Crypto("Bitcoin", 1000.0))
+    println("Offering " + offerBitCoin + " for BitCoin")
+    val offerRealEstate = getOffer(RealEstate("House", 1000000.0, 2000.0))
+    println("Offering " + offerRealEstate + " for House")
+    val offerMutualFund = getOffer(MutualFund("SP500", 2000.0, "lowrisk"))
+    println("Offering " + offerMutualFund + " for Mutual Fund")
+
+  }
+
+   // TOOD: Complete this: def getOffer(investment: Investment): Double = investment match {
+   //}
+
 }
--- 07-caseclasses/src/test/scala/example/InvestmentValuationSpec.scala
+++
@@ -1,9 +0,0 @@
-package example
-
-import org.scalatest._
-
-class InvestmentValuationSpec extends FlatSpec with Matchers {
-  "The InvestmentValuation object" should "use case classes" in {
-    InvestmentValuation.goal shouldEqual "use case classes"
-  }
-}
