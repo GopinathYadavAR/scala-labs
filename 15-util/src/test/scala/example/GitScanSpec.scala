7dbc74a422906b4630131509950026b0a06b1129
Mark Kerzner <mark@elephantscale.com>
2018-02-16 16:08:31 -0600

    SCALA: get scanning experiements [1]

---
+++ 15-util/src/test/scala/example/GitScanSpec.scala
@@ -0,0 +1,9 @@
+package example
+
+import org.scalatest._
+
+class GitScanSpec extends FlatSpec with Matchers {
+  "The GitScan object" should "scan a git dir" in {
+    GitScan.status shouldEqual "OK"
+  }
+}
